e274e9d907d5affbd23dd2c3b264eab4bfb62962
Kelvin Snepvangers <kelvinsnepvangers@gmail.com>
2018-12-12 01:11:44 +0100

    added auth to routes

--- routes/trainingschedule.routes.js
+++ routes/trainingschedule.routes.js
@@ -2,84 +2,100 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require("../db/mongoose");
 
-//TODO: add authentication (maybe a token)
-
 router.get('/', (req, res) => {
-  mongoose.Trainingschedule.find((err, result) => {
-    if (err) return console.error(err);
-    res.send(result);
+  var token = (req.header('X-Access-Token')) || '';
+  auth.decodeToken(token, (err, payload) => {
+    if (err) {
+      console.log('Error handler: ' + err.message);
+      res.status((err.status || 401));
+    } else {
+      mongoose.Trainingschedule.find((err, result) => {
+        if (err) return console.error(err);
+        res.send(result);
+      })
+    }
   })
 });
 
 router.get('/:id', (req, res) => {
-  if(req.params["id"]){
-    mongoose.Trainingschedule.findOne({_id: req.params["id"]}, (err, result) => {
-      if (err) return console.error(err);
-      res.send(result);
-    })
-  }
-});
-
-router.get('/user/:id', (req, res) => {
-  if(req.params["id"]){
-    mongoose.Trainingschedule.find({user: req.params["id"]}, (err, result) => {
-      if (err) return console.error(err);
-      res.send(result);
+  var token = (req.header('X-Access-Token')) || '';
+  if (req.params["id"]) {
+    auth.decodeToken(token, (err, payload) => {
+      if (err) {
+        console.log('Error handler: ' + err.message);
+        res.status((err.status || 401));
+      } else {
+        mongoose.Trainingschedule.findOne({ _id: req.params["id"] }, (err, result) => {
+          if (err) return console.error(err);
+          res.send(result);
+        })
+      }
     })
   }
 });
 
 router.post('/', (req, res) => {
-  if(req.body.username && req.body.name){
-    mongoose.User.find({username: req.body.username}, (err, result) =>{
-      if (err) return console.error(err);
-      if(result){
-        var sportschedule = new mongoose.Trainingschedule({ user: result._id, name: req.body.name})
+  var token = (req.header('X-Access-Token')) || '';
+  if (req.body.name) {
+    auth.decodeToken(token, (err, payload) => {
+      if (err) {
+        console.log('Error handler: ' + err.message);
+        res.status((err.status || 401));
+      } else {
+        var sportschedule = new mongoose.Trainingschedule({ user: result._id, name: req.body.name })
         sportschedule.save(() => {
           res.send(sportschedule);
         });
-      } else{
-        res.send("User doenst exist");
       }
     });
-  } else{
+  } else {
     res.send("Wrong post body");
   }
 });
 
-router.put("/:id/:day", (req,res) =>{
-  if(req.params["id"] && req.body.exercise && req.params["day"]){
-    mongoose.Trainingschedule.findOne({_id: req.params["id"]}, (err, trainingschedule) =>{
-      if (err) return console.error(err);
-      if(trainingschedule){
-        trainingschedule.days[req.params["day"]] = (req.body.exercise);
-        trainingschedule.save(() => {
-          res.send(trainingschedule);
+router.put("/:id/:day", (req, res) => {
+  var token = (req.header('X-Access-Token')) || '';
+  if (req.params["id"] && req.body.exercise && req.params["day"]) {
+    auth.decodeToken(token, (err, payload) => {
+      if (err) {
+        console.log('Error handler: ' + err.message);
+        res.status((err.status || 401));
+      } else {
+        mongoose.Trainingschedule.findOne({ user: payload.sub_id, _id: req.params["id"] }, (err, trainingschedule) => {
+          if (err) return console.error(err);
+          if (trainingschedule) {
+            trainingschedule.days[req.params["day"]] = (req.body.exercise);
+            trainingschedule.save(() => {
+              res.send(trainingschedule);
+            });
+          } else {
+            res.send(401);
+          }
         });
-      } else{
-        res.send(401);
       }
-    });
-  } else{
+    })
+  } else {
     res.send(401);
   }
 });
 
-// router.delete("/", (req,res) =>{
-//   if(req.body.username && req.body.password){
-//     mongoose.User.findOne({username: req.body.username, password: req.body.password}, (err, user) =>{
-//       if (err) return console.error(err);
-//       if(user){
-//         user.remove(() => {
-//           res.send(user);
-//         });
-//       } else{
-//         res.send(401);
-//       }
-//     });
-//   } else{
-//     res.send(401);
-//   }
-// });
+router.delete("/:id", (req, res) => {
+  var token = (req.header('X-Access-Token')) || '';
+  if (req.params["id"]) {
+    auth.decodeToken(token, (err, payload) => {
+      if (err) {
+        console.log('Error handler: ' + err.message);
+        res.status((err.status || 401));
+      } else {
+        mongoose.Trainingschedule.deleteOne({ user: payload.sub, _id: req.params["id"] }, (err) => {
+          if (err) return console.error(err);
+          return req.params["id"];
+        });
+      }
+    });
+  } else {
+    res.send(401);
+  }
+});
 
 module.exports = router;
